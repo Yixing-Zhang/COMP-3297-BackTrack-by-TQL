21fc9d0f41a6ee06aa92096bf7835bdad61b106b
Zhang_Yixing <u3544946@connect.hku.hk>
2019-10-24 20:44:22 +0800

    Model and set up the database for sprint 1.

--- BackTrack/productBacklog/admin.py
+++ BackTrack/productBacklog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Manager)
+admin.site.register(Project)
+admin.site.register(Developer)
+admin.site.register(ProductBacklog)
+admin.site.register(PBI)
