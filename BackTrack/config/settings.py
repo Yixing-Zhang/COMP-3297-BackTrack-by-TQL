589b54f9204e4abfee0c1b74aedf05480b955909
Zhang_Yixing <u3544946@connect.hku.hk>
2019-10-25 11:54:54 +0800

    Update settings.

--- BackTrack/config/settings.py
+++ BackTrack/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+iy#d$l-0-anlo)d%gaf6%dtz0svh37mq^8=-8=jqkb42685$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Hongkong'
 
 USE_I18N = True
 
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
